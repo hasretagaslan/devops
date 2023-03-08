ed635bdebed5639c6e5a6fc93556147ab26d89c0
hasretagaslan <hasretagaslan@gmail.com>
2023-03-09 01:15:27 +0300

    uploadAllFile

---
+++ RestApi/src/main/java/Galaksiya/RestApi/controller/HosePriceController.java
@@ -0,0 +1,51 @@
+package Galaksiya.RestApi.controller;
+
+import Galaksiya.RestApi.model.HousePrice;
+
+import Galaksiya.RestApi.repository.HouseRepository;
+import lombok.Data;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(consumes = "application/json; charset=utf-8")
+@ResponseBody
+@Data
+public class HosePriceController {
+    private final HouseRepository houseRepository;
+
+    @GetMapping("/findAllHouse")
+    public List<HousePrice> getHouse() {
+        return houseRepository.findAll();
+    }
+
+    @PostMapping(path = "/addNewHouse")
+    public HousePrice createHouse(@RequestBody HousePrice HousePrice) {
+        return houseRepository.save(HousePrice);
+    }
+
+    @PutMapping("/updateHouse/{id}")
+    public ResponseEntity<HousePrice> updateHouse(@PathVariable String id, @RequestBody HousePrice house) {
+        HousePrice updateHouse = houseRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("House not exist with id: " + id));
+        updateHouse.setProperty_Name(house.getProperty_Name());
+        updateHouse.setPrice(house.getPrice());
+        updateHouse.setHouse_Type(house.getHouse_Type());
+        updateHouse.setArea_in_sq_ft(house.getArea_in_sq_ft());
+        updateHouse.setNo_of_Bedrooms(house.getNo_of_Bedrooms());
+        updateHouse.setNo_of_Bathrooms(house.getNo_of_Bathrooms());
+        updateHouse.setNo_of_Receptions(house.getNo_of_Receptions());
+        updateHouse.setLocation(house.getLocation());
+        updateHouse.setCity_Country(house.getCity_Country());
+        updateHouse.setPostal_Code(house.getPostal_Code());
+        houseRepository.save(updateHouse);
+        return ResponseEntity.ok(updateHouse);
+    }
+}
+
+
+
